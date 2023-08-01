c4e6de44ba24309b655c0dba31a32694d59c486a
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 15:58:08 +0530

    Tournament method added

--- 2_Min_Max/main.cpp
+++ 2_Min_Max/main.cpp
@@ -19,47 +19,88 @@ struct Pair{
 
 //Method 2: Linear search
 
-Pair Min_Max(int *arr,int n)
-{
-    Pair minmax;
-    if(n==1)
-    {
-        minmax.min=arr[0];
-        minmax.max=arr[0];
-        return minmax;
-    }
-
-        if(arr[0]>arr[1])
-        {
-            minmax.min=arr[1];
-            minmax.max=arr[0];
-        }
-        else
-        {
-            minmax.min=arr[0];
-            minmax.max=arr[1];
-        }
-
-
-        for(int i=2;i<n;i++)
-        {
-            if(arr[i]>minmax.max)
-            {
-                minmax.max=arr[i];
-            }
-            else if(arr[i]<minmax.min)
-            {
-                minmax.min=arr[i];
-            }
-        }
-
-
-    return minmax;
-}
+//Pair Min_Max(int *arr,int n)
+//{
+//    Pair minmax;
+//    if(n==1)
+//    {
+//        minmax.min=arr[0];
+//        minmax.max=arr[0];
+//        return minmax;
+//    }
+//
+//        if(arr[0]>arr[1])
+//        {
+//            minmax.min=arr[1];
+//            minmax.max=arr[0];
+//        }
+//        else
+//        {
+//            minmax.min=arr[0];
+//            minmax.max=arr[1];
+//        }
+//
+//
+//        for(int i=2;i<n;i++)
+//        {
+//            if(arr[i]>minmax.max)
+//            {
+//                minmax.max=arr[i];
+//            }
+//            else if(arr[i]<minmax.min)
+//            {
+//                minmax.min=arr[i];
+//            }
+//        }
+//
+//
+//    return minmax;
+//}
 
 //Method 3: Tournament Method
 
+Pair Min_Max(int *arr, int n,int l,int h)
+{
+     Pair minmax,mml,mmr;
+     int mid;
 
+     if(l==h)
+     {
+         minmax.min=arr[l];
+         minmax.max=arr[l];
+         return minmax;
+     }
+     if(h==l+1)
+     {
+         if(arr[l]>arr[h])
+         {
+             minmax.min=arr[h];
+             minmax.max=arr[l];
+         }
+         else
+         {
+             minmax.min=arr[l];
+             minmax.max=arr[h];
+         }
+     }
+
+     mid = (l+h)/2;
+     mml = Min_Max(arr,n,l,mid);
+     mmr = Min_Max(arr,n,mid+1,h);
+
+     if(mml.min<mmr.min)
+         minmax.min=mml.min;
+     else
+         minmax.min=mmr.min;
+
+     if(mml.max>mmr.max)
+         minmax.max=mml.max;
+     else
+         minmax.max=mmr.max;
+
+     return minmax;
+
+}
 
 int main()
 {
@@ -70,7 +111,7 @@ int main()
     {
         cin>>arr[i];
     }
-    Pair ans = Min_Max(arr,n);
+    Pair ans = Min_Max(arr,n,0,n-1);
     cout<<"Min:"<<ans.min<<endl;
     cout<<"Max:"<<ans.max<<endl;
 
