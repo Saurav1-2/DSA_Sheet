0e08559df8a17a2fb20687db501c3a27e506c8c9
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 15:20:51 +0530

    Code for reversing array using swap function

--- 1_Reverse_Array/main.cpp
+++ 1_Reverse_Array/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void reverse_arr(int *arr, int n)
+{
+    int start = 0, end = n-1;
+    while(start<=end)
+        swap(arr[start++],arr[end--]);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    reverse_arr(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
