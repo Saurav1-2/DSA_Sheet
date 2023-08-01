5cfedf1ede40e0702d5622d9fb455167400c2bce
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 16:14:30 +0530

    Sorting approach

--- 3_Kth_Min_Max/main.cpp
+++ 3_Kth_Min_Max/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+struct Pair{
+    int min;
+    int max;
+};
+//Method 1: Sorting
+Pair Kth_Min_Max(int arr[],int n,int k)
+{
+    sort(arr,arr+n);
+    Pair minmax{};
+    minmax.min=arr[k-1];
+    minmax.max=arr[n-k];
+
+    return minmax;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    Pair minmax = Kth_Min_Max(arr,n,k);
+    cout<<"Min:"<<minmax.min<<endl;
+    cout<<"Max:"<<minmax.max<<endl;
+
     return 0;
 }
