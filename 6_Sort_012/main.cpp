e45ac075b059a108664eab7c976c970dea2cf6ec
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 20:33:54 +0530

    Pointer approach or Ducth National Flag Aglo

--- 6_Sort_012/main.cpp
+++ 6_Sort_012/main.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 void sort_012(int *arr,int n)
 {
-    
+    int l=0;
+    int h=n-1;
+    for(int i=0;i<n && i<=h;)
+    {
+        if(arr[i]==0)
+        {
+            swap(arr[i],arr[l]);
+            i++;
+            l++;
+        }
+        else if(arr[i]==2)
+        {
+            swap(arr[i],arr[h]);
+            h--;
+        }
+        else
+            i++;
+    }
 }
 
 
@@ -16,5 +33,11 @@ int main()
     {
         cin>>arr[i];
     }
+    sort_012(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 
 }
