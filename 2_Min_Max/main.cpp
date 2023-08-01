38341eb8560d06c6920f14bf5dc53d9dc3ee66d2
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 15:46:36 +0530

    Linear search method added

--- 2_Min_Max/main.cpp
+++ 2_Min_Max/main.cpp
@@ -2,20 +2,64 @@
 using namespace std;
 
 struct Pair{
-    int min;
-    int max;
+    int min=INT_MAX;
+    int max=INT_MIN;
 };
+//Method 1: Sorting the array
+//Pair Min_Max(int *arr,int n)
+//{
+//    sort(arr,arr+n);
+//    Pair ans;
+//    ans.min = arr[0];
+//    ans.max=arr[n-1];
+//
+//    return ans;
+//
+//}
+
+//Method 2: Linear search
 
 Pair Min_Max(int *arr,int n)
 {
-    sort(arr,arr+n);
-    Pair ans;
-    ans.min = arr[0];
-    ans.max=arr[n-1];
+    Pair minmax;
+    if(n==1)
+    {
+        minmax.min=arr[0];
+        minmax.max=arr[0];
+        return minmax;
+    }
 
-    return ans;
+        if(arr[0]>arr[1])
+        {
+            minmax.min=arr[1];
+            minmax.max=arr[0];
+        }
+        else
+        {
+            minmax.min=arr[0];
+            minmax.max=arr[1];
+        }
 
+
+        for(int i=2;i<n;i++)
+        {
+            if(arr[i]>minmax.max)
+            {
+                minmax.max=arr[i];
+            }
+            else if(arr[i]<minmax.min)
+            {
+                minmax.min=arr[i];
+            }
+        }
+
+
+    return minmax;
 }
+
+//Method 3: Tournament Method
+
+
 
 int main()
 {
