3690017e8abf33d4c198de138d3d203dd7b9a4ea
Saurav1-2 <sk5741467@gmail.com>
2023-08-01 20:18:17 +0530

    Sort 0 1 2

--- 6_Sort_012/main.cpp
+++ 6_Sort_012/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void sort_012(int *arr,int n)
+{
+    
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
 }
