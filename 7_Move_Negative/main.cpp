816fe42d8a036537ef1a2b34fe6981d04438f985
Saurav1-2 <sk5741467@gmail.com>
2023-08-03 20:14:12 +0530

    Two pointer appraoch

--- 7_Move_Negative/main.cpp
+++ 7_Move_Negative/main.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+void shift_neg(int *arr, int n)
+{
+    int l=0,r=n-1;
+    while(l<=r)
+    {
+        if(arr[l]<0 && arr[r]<0)
+        {
+            l++;
+        }
+        else if(arr[l]>=0 && arr[r]<0)
+        {
+            swap(arr[l],arr[r]);
+            l++;
+            r--;
+        }
+        else if(arr[l]>0 && arr[r]>0)
+        {
+            r--;
+        }
+        else
+        {
+            r--;
+            l++;
+        }
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    shift_neg(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
+
 }
